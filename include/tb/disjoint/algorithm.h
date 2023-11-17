31636125bdea07028babd54c9dac6cac7cef161e
tbrekalo <t@tbrekalo.xyz>
2023-11-17 16:15:26 +0100

    binary-split(wip): making subset constexpr

--- include/tb/disjoint/algorithm.h
+++ include/tb/disjoint/algorithm.h
@@ -10,28 +10,30 @@ class subset {
   std::vector<std::uint8_t> state_;
 
  public:
-  explicit subset(std::size_t size) : state_(size) {}
+  constexpr explicit subset(std::size_t size) : state_(size) {}
 
-  subset() = default;
-  ~subset() = default;
+  constexpr subset() = default;
+  constexpr ~subset() = default;
 
-  subset(subset const&) = default;
-  auto operator=(subset const&) -> subset& = default;
+  constexpr subset(subset const&) = default;
+  constexpr auto operator=(subset const&) -> subset& = default;
 
-  subset(subset&&) = default;
-  auto operator=(subset&&) -> subset& = default;
+  constexpr subset(subset&&) = default;
+  constexpr auto operator=(subset&&) -> subset& = default;
 
-  friend auto operator<=>(subset const&, subset const&) = default;
+  constexpr friend auto operator<=>(subset const&, subset const&) = default;
 
-  auto operator[](std::size_t idx) const noexcept -> bool {
+  constexpr auto operator[](std::size_t idx) const noexcept -> bool {
     return state_[idx];
   }
-  auto contains(std::size_t idx) const noexcept -> bool { return state_[idx]; }
-  auto size() const noexcept -> std::size_t { return state_.size(); }
+  constexpr auto contains(std::size_t idx) const noexcept -> bool {
+    return state_[idx];
+  }
+  constexpr auto size() const noexcept -> std::size_t { return state_.size(); }
 
-  auto insert(std::size_t idx) noexcept -> void { state_[idx] = 1; }
-  auto erase(std::size_t idx) noexcept -> void { state_[idx] = 0; }
-  auto clear() noexcept -> void { std::ranges::fill(state_, 0); }
+  constexpr auto insert(std::size_t idx) noexcept -> void { state_[idx] = 1; }
+  constexpr auto erase(std::size_t idx) noexcept -> void { state_[idx] = 0; }
+  constexpr auto clear() noexcept -> void { std::ranges::fill(state_, 0); }
 };
 
 auto binary_split(std::vector<subset>) -> std::vector<subset>;
