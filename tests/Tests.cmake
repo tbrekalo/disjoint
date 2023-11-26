545d91f9110ee8f915c6a3a93c1f5596d6b0caae
tbrekalo <t@tbrekalo.xyz>
2023-11-26 17:10:26 +0100

    hash: updating CMake and adding ankerl::unordered_dense dependency

--- tests/Tests.cmake
+++ tests/Tests.cmake
@@ -1,16 +1,14 @@
-include(FetchContent)
-
-set(DISJOINT_FETCH_TARGETS "")
+set(DISJOINT_TEST_FETCH_TARGETS "")
 
 find_package(doctest 2.4 QUIET)
 if(NOT doctest_FOUND)
   FetchContent_Declare(
     doctest
-    GIT_REPOSITORY https://github.com/doctest/doctest.git
+    GIT_REPOSITORY git@github.com:doctest/doctest.git
     GIT_TAG v2.4.11
     GIT_SHALLOW TRUE)
 
-  list(APPEND DISJOINT_FETCH_TARGETS doctest)
+  list(APPEND DISJOINT_TEST_FETCH_TARGETS doctest)
 endif()
 
 find_package(nanobench 4.1 QUIET)
@@ -21,9 +19,9 @@ if(NOT nanobench_FOUND)
     GIT_TAG v4.1.0
     GIT_SHALLOW TRUE)
 
-  list(APPEND DISJOINT_FETCH_TARGETS nanobench)
+  list(APPEND DISJOINT_TEST_FETCH_TARGETS nanobench)
 endif()
 
-FetchContent_MakeAvailable(${DISJOINT_FETCH_TARGETS})
+FetchContent_MakeAvailable(${DISJOINT_TEST_FETCH_TARGETS})
 add_executable(disjoint-test ${CMAKE_CURRENT_LIST_DIR}/src/main.cc)
 target_link_libraries(disjoint-test PRIVATE disjoint doctest::doctest nanobench)
